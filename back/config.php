0fe2541ed44af9d544a99d12406066000b344729
ari dehmani <arijarijdehmani@gmail.com>
2026-04-27 14:48:27 -0700

    update project

--- back/config.php
+++ back/config.php
@@ -4,7 +4,7 @@ define('DB_HOST', 'localhost');
 define('DB_USER', 'root');
 define('DB_PASS', '');
 define('DB_NAME', 'coffeeshop');
-function connection(){
+function getConnection(){
     $conn = new mysqli(DB_HOST, DB_USER, DB_PASS, DB_NAME); //instance  : creation d un de connexion a la base de données oop constructeur de la classe mysqli
     if ($conn->connect_error) {
         die(json_encode(["error" => "Connection failed: " . $conn->connect_error])); //pour la convertie en objet json 
@@ -20,7 +20,7 @@ function isAdmin(){
 function isLoggedIn(){
     return isset($_SESSION['user_id']); //determine si le variable est declare et n  est pas null 
 }
-function redirect(){
+function redirectIfNotAdmin(){
     if (!isAdmin()) {
         header('Location: ../auth.php');
         exit;
